ccd0408c9c10004703ac77233bcc3e0ab9013d17
YASİN IRBIK <irbikyasin@gmail.com>
2021-03-17 21:08:19 +0300

    17.03.2021

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        //[HttpGet]
+        //public string Get()
+        //{
+        //    return "Merhaba";
+        //} 
+        //Loosely Coupled
+      //  naming Convention
+      //IOC Container(inversion of Control)
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public List<Product> Get()
+        {
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
---
+++ WebAPI/Startup.cs
@@ -0,0 +1,65 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Autofac,Nnjecct,CastleWindsor,tructureMap,LightInject,DryImject --> IoC Container
+            //AOP
+            services.AddControllersWithViews();
+            services.AddSingleton<IProductService,ProductManager>();//Bu kod satýrý default deðil biz yazdýk. bu satýrýn amacý,IProdcutService istendiðinde ProductManger oluþtru ve ona ver yani eðer ki IProdcutService tipinde baðýmlýlýk görürse ProductManager onun karþýlýðýdýr demektir.
+            services.AddSingleton<IProductDal, EfProductDal>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
